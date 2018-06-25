ec31624b48dc4754d7d87a0bd07918f8dfb128ca
IgorkoGR <igor47xgrabar@gmail.com>
2018-06-25 04:24:27 +0300

    Fixed a lot of stuff!

--- guessinggame.sh
+++ guessinggame.sh
@@ -5,14 +5,25 @@ num_of_files=$(ls -1 | wc -l)
 
 user_input=-1
 
-while [ $user_input -ne $num_of_files ]
-do
-	read user_input
-	if [ $user_input -gt $num_of_files ]
-	then
+echo "Guess how many files there are in this directory:"
+
+function tell_user_they_are_stupid {
+	if [ $1 -gt $2 ]
+	then 
 		echo "Too big!"
 	else
 		echo "Too small!"
+	fi
+}
+
+while [[ $user_input -ne $num_of_files ]]
+do
+	read user_input
+	if [[ $user_input != "" ]]
+	then
+		tell_user_they_are_stupid $user_input $num_of_files
+	else
+		echo "Please input a number!"
 	fi
 done
 
