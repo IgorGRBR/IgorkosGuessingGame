9e7147a6d15edf6634629b64217a978bde52c69d
IgorkoGR <igor47xgrabar@gmail.com>
2018-06-25 04:07:53 +0300

    Now it works!

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,6 +1,19 @@
 #Here goes the game
 
 #soo first we get a number...
-num_of_files=$(ls $0 -1 | wc -l)
-echo $num_of_files
+num_of_files=$(ls -1 | wc -l)
 
+user_input=-1
+
+while [ $user_input -ne $num_of_files ]
+do
+	read user_input
+	if [ $user_input -gt $num_of_files ]
+	then
+		echo "Too big!"
+	else
+		echo "Too small!"
+	fi
+done
+
+echo "Correct!"
